9c45a5fb8e480db5566199bf01aefc7011eae0dd
ZLM <2113743321@qq.com>
2021-08-10 17:43:12 +0800

    完成

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
 const displayLeft = document.querySelector('.display__time-left')
-const displayEndTime = document.querySelector('.display__end-time')
+const displayEnd = document.querySelector('.display__end-time')
+const buttons = document.querySelectorAll('[data-time]')
+let countdown
 
-
-function buttonHandler() {
-    // console.log(this.dataset.time);
-    const seconds = this.dataset.time
-    const hour = Math.floor(seconds / 3600)
-    const min = Math.floor(seconds % 3600 / 60)
-    const sec = Math.floor(seconds % 3600 % 60)
-        // console.log(hour);
-        // console.log(min);
-    displayLeft.textContent = `${hour<10?'0'+hour:''}:${min<10?'0'+min:min}:${sec<10?'0'+sec:sec}`
-
-    const now = new Date()
+function timer(seconds) {
+    clearInterval(countdown)
+    const now = Date.now()
     console.log(now);
     const then = now + seconds * 1000
     console.log(then);
+    displayLeftTime(seconds)
+    displayEndTime(then)
+
+    countdown = setInterval(() => {
+
+        const secondsLeft = Math.round((then - Date.now()) / 1000)
+
+        if (secondsLeft < 0) {
+            clearInterval(countdown)
+            return
+        }
+        displayLeftTime(secondsLeft)
+    }, 1000)
+
+}
+
+function displayLeftTime(seconds) {
+    const hour = Math.floor(seconds / 3600)
+    const min = Math.floor(seconds % 3600 / 60)
+    const sec = Math.floor(seconds % 3600 % 60)
+    displayLeft.textContent = `${hour<10?'0'+hour:''}:${min<10?'0'+min:min}:${sec<10?'0'+sec:sec}`
+}
+
+function displayEndTime(then) {
     const newDate = new Date(then)
     const laterHour = newDate.getHours()
     const laterMin = newDate.getMinutes()
     const endTime = `结束后时间是${laterHour<10?'0'+laterHour:laterHour}:${laterMin<10?'0'+laterMin:laterMin}`
-    displayEndTime.textContent = endTime
+    displayEnd.textContent = endTime
 }
-const buttons = document.querySelectorAll('[data-time]')
+
+function buttonHandler() {
+    // console.log(this.dataset.time);
+    const seconds = this.dataset.time
+    timer(seconds)
+
+}
+
+
 buttons.forEach(button => button.addEventListener('click', buttonHandler))
+
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    timer(mins * 60);
+    this.reset();
+})
